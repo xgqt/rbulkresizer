91febc87a82ef17fa440f8ec342b16e323ce7aa5
Maciej Barć <xgqt@riseup.net>
2021-05-24 00:41:18 +0200

    info.rkt: bump to 1.3.0
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

--- info.rkt
+++ info.rkt
@@ -25,7 +25,7 @@
 
 (define pkg-authors '(Maciej Barć))
 (define pkg-desc "Graphical bulk picture resize tool.")
-(define version "1.2.3")
+(define version "1.3.0")
 
 (define collection 'multi)
 (define scribblings '(("scribblings/rbulkresizer.scrbl" ())))
